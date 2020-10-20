887f945dd1f9ee203092ad8a0d7e7a216f6445c5
RafaelSantosOliveira <rsrafael2014@hotmail.com>
2020-10-20 14:24:42 -0300

    Inicialização do projeto

---
+++ src/test/java/com/example/LoginProjetor/LoginProjetorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoginProjetor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginProjetorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
