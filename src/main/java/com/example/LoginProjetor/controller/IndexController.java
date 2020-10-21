6c2715e4610a01160f32952df1116447c3902cde
RafaelSantosOliveira <rsrafael2014@hotmail.com>
2020-10-21 14:35:27 -0300

    make it better

--- src/main/java/com/example/LoginProjetor/controller/IndexController.java
+++ src/main/java/com/example/LoginProjetor/controller/IndexController.java
@@ -1,8 +1,19 @@
 package com.example.LoginProjetor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String getLogin() {
+		return "login";
+	}
 
 }
